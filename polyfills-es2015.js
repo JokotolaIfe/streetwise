38eb5558ce8476e7200210c9474e5829f3467f78
Jokotola Ifeoluwa (C) <Jokotola.Ifeoluwa@fidelitybank.ng>
2021-03-26 12:07:42 +0100

    Corrections implemented

--- polyfills-es2015.js
+++ polyfills-es2015.js
@@ -6151,8 +6151,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\HP\Desktop\MY PROJECTS\Streetwise\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\HP\Desktop\MY PROJECTS\Streetwise\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Jokotola.Ifeoluwa\Desktop\ifeoluwa\street\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Jokotola.Ifeoluwa\Desktop\ifeoluwa\street\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
